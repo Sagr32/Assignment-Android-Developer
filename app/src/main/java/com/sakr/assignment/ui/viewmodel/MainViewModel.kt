b4f48521cc933a215a890bfdd2f91e6d8910d9b4
sagr32 <sagr3272@gmail.com>
2022-11-20 23:47:02 +0200

    Injected MainViewModel

--- app/src/main/java/com/sakr/assignment/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/sakr/assignment/ui/viewmodel/MainViewModel.kt
@@ -4,11 +4,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sakr.assignment.data.remote.ApiStatus
 import com.sakr.assignment.data.repository.MainRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MainViewModel(private val repository: MainRepository) : ViewModel() {
+@HiltViewModel
+class MainViewModel @Inject constructor(private val repository: MainRepository) : ViewModel() {
 
     sealed class NewsEvent {
         class Success<T>(val result: T) : NewsEvent()
