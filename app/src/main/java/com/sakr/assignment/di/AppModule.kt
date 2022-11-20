0c9979e032d0034e211516eed57ba6a2c1248fb0
sagr32 <sagr3272@gmail.com>
2022-11-20 23:31:09 +0200

    Injected Retrofit builder using HILT

--- app/src/main/java/com/sakr/assignment/di/AppModule.kt
+++ app/src/main/java/com/sakr/assignment/di/AppModule.kt
@@ -1,9 +1,12 @@
 package com.sakr.assignment.di
 
+import com.sakr.assignment.data.remote.ApiInterface
+import com.sakr.assignment.utils.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
@@ -16,6 +19,15 @@ class AppModule {
     @Provides
     fun provideGSON(): GsonConverterFactory {
         return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(gson: GsonConverterFactory): ApiInterface {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(gson)
+            .build().create(ApiInterface::class.java)
     }
 
 
