58b6dbbfa1877bbcc64ffc55ff130b230cee11bc
sagr32 <sagr3272@gmail.com>
2022-11-20 23:29:30 +0200

    Injected GSON using HILT

--- app/src/main/java/com/sakr/assignment/di/AppModule.kt
+++ app/src/main/java/com/sakr/assignment/di/AppModule.kt
@@ -1,2 +1,22 @@
 package com.sakr.assignment.di
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideGSON(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+
+}
