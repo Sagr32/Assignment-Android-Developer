cccb9f07421bfdb102c33d158be4bff4eb8a6ffc
sagr32 <sagr3272@gmail.com>
2022-11-21 10:52:15 +0200

    Added RegistrationFragment file

---
+++ app/src/main/java/com/sakr/assignment/RegistrationFragment.kt
@@ -0,0 +1,22 @@
+package com.sakr.assignment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sakr.assignment.databinding.FragmentRegistrationBinding
+
+
+class RegistrationFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentRegistrationBinding.inflate(inflater)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+}
