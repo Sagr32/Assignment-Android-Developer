2df2e5a7ce8c272e15b0cffdca64b41d7fe57e8a
sagr32 <sagr3272@gmail.com>
2022-11-21 20:38:17 +0200

    Addded Webview bottom sheet in details

--- app/src/main/java/com/sakr/assignment/ui/view/ArticleDetailsFragment.kt
+++ app/src/main/java/com/sakr/assignment/ui/view/ArticleDetailsFragment.kt
@@ -5,12 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.sakr.assignment.R
 import com.sakr.assignment.databinding.FragmentArticleDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ArticleDetailsFragment : Fragment() {
+    lateinit var progressBar: ProgressBar
     private val args: ArticleDetailsFragmentArgs by navArgs()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +25,34 @@ class ArticleDetailsFragment : Fragment() {
         val binding = FragmentArticleDetailsBinding.inflate(inflater)
         binding.article = args.article
         // Inflate the layout for this fragment
+        binding.openUrlBtn.setOnClickListener {
+            showBottomSheetDialog(args.article.url)
+        }
         return binding.root
+    }
+
+
+    private fun showBottomSheetDialog(url: String) {
+        val bottomSheetDialog = BottomSheetDialog(requireContext())
+        bottomSheetDialog.setContentView(R.layout.webview_bottom_sheet)
+        val webView = bottomSheetDialog.findViewById<WebView>(R.id.webView)
+        progressBar = bottomSheetDialog.findViewById<ProgressBar>(R.id.progressBar)!!
+        bottomSheetDialog.show()
+        webView?.webViewClient = MyWebViewClient()
+        webView?.settings?.setJavaScriptEnabled(true)
+        webView?.loadUrl(url)
+
+    }
+
+    inner class MyWebViewClient : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            view.loadUrl(url)
+            return true
+        }
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+            progressBar.visibility = View.GONE
+        }
     }
 }
