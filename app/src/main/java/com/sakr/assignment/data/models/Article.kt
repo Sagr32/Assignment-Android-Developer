64bc3d182615892464d9bea62c2361628d4e1434
sagr32 <sagr3272@gmail.com>
2022-11-21 22:06:44 +0200

    Added Article Entity

--- app/src/main/java/com/sakr/assignment/data/models/Article.kt
+++ app/src/main/java/com/sakr/assignment/data/models/Article.kt
@@ -3,25 +3,46 @@ package com.sakr.assignment.data.models
 
 import kotlinx.parcelize.Parcelize
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+import com.sakr.assignment.utils.Constants.ARTICLE_TABLE
 
 
 @Parcelize
-data class Article(
+@Entity(tableName = ARTICLE_TABLE)
+data class Article @JvmOverloads constructor(
     @SerializedName("author")
+    @ColumnInfo(name = "author")
     val author: String? = "",
+
+    @ColumnInfo(name = "content")
     @SerializedName("content")
     val content: String? = "",
+
+    @ColumnInfo(name = "description")
     @SerializedName("description")
     val description: String,
+
+    @ColumnInfo(name = "publishedAt")
     @SerializedName("publishedAt")
     val publishedAt: String,
+
+    @Ignore
     @SerializedName("source")
     val source: SourceX,
+
+    @PrimaryKey()
     @SerializedName("url")
     val url: String,
+
+    @ColumnInfo(name = "title")
     @SerializedName("title")
     val title: String,
+
+    @ColumnInfo(name = "urlToImage")
     @SerializedName("urlToImage")
     val urlToImage: String
 ) : Parcelable
--- app/src/main/java/com/sakr/assignment/utils/Constants.kt
+++ app/src/main/java/com/sakr/assignment/utils/Constants.kt
@@ -4,4 +4,5 @@ object Constants {
     const val BASE_URL = "https://newsapi.org"
     const val API_KEY = "344e477ac241420db1e7501341b3ff48"
     const val USER_TABLE = "User_Table"
+    const val ARTICLE_TABLE = "Article_Table"
 }
