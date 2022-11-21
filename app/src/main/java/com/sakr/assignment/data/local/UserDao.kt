57ebc7e1a05b640d3a4635e0f82363ed3117b9ba
sagr32 <sagr3272@gmail.com>
2022-11-21 14:43:18 +0200

    Added User DAO

---
+++ app/src/main/java/com/sakr/assignment/data/local/UserDao.kt
@@ -0,0 +1,30 @@
+package com.sakr.assignment.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+
+@Dao
+interface UsersDao {
+
+    //for single user insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertUser(user: User): Long
+
+    //checking user exist or not in our db
+    @Query("SELECT * FROM User_Table WHERE email LIKE :email AND password LIKE :password")
+    fun readLoginData(email: String, password: String): User
+
+
+    //getting user data details
+    @Query("select * from User_Table where id Like :id")
+    fun getUserDataDetails(id: Long): User
+
+    //deleting all user from db
+    @Query("DELETE FROM User_Table")
+    fun deleteAll()
+
+
+}
